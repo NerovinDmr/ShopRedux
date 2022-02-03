8555d18936c7c92f25fc124676cbf9d30bf46c5f
Сергей <47758071+NerovinDmr@users.noreply.github.com>
2022-02-03 21:11:48 +0300

    Update actions.js

--- src/store/cart/actions.js
+++ src/store/cart/actions.js
@@ -15,18 +15,4 @@ export const cartActions = {
     setPlusQty: (id) => ({ type: cartActionTypes.SET_PLUS_QTY, payload: id }),
     setMinusQty: (id) => ({ type: cartActionTypes.SET_MINUS_QTY, payload: id }),
     removeFromCart: (id) => ({ type: cartActionTypes.REMOVE_FROM_CART, payload: id }),
-    fetchCart: () => async (dispatch) => {
-        dispatch(cartActionTypes.setLoading(true));
-
-        try {
-            const response = await fetch('http://localhost:3000/ShowcaseDB.json');
-            const cartItems = await response.json();
-
-            dispatch(cartActionTypes.addCartItem(cartItems));
-        } catch (error) {
-            console.log(error.message);
-        } finally {
-            dispatch(cartActionTypes.setLoading(false));
-        }
-    }
 }
